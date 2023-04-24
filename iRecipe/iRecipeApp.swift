449395430e614e28c2bd28bd7bab5ad84d55fb3d
Kacique <wbrand90@gmail.com>
2023-04-24 07:49:16 -0700

    Initial Commit

---
+++ iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by William on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
